ba14a56d5370c4e00e03fcf22aca8ac94d32c347
Luong <luongnguyentrongduc@gmail.com>
2023-05-29 13:28:54 +0700

    provision superset pdt

---
+++ provision_and_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Read unit_name from command-line argument
+unit_name="$1"
+
+# Call Python script with unit_name argument
+python provision_superset.py "$unit_name"
+
+# Create commit with changes
+git add .
+git commit -m "provision superset $unit_name"
+
+# Push changes to GitHub repository
+git push origin master

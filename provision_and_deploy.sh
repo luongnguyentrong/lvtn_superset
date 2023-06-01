25221e6eb6eb07293b7a58beab64012e74d4be0e
Luong <luongnguyentrongduc@gmail.com>
2023-06-01 21:19:34 +0700

    provision superset pdt

--- provision_and_deploy.sh
+++ provision_and_deploy.sh
@@ -9,9 +9,9 @@ unit_name="$1"
 # Call Python script with unit_name argument
 python provision_superset.py "$unit_name"
 
-# Create commit with changes
-git add .
-git commit -m "provision superset $unit_name"
+# # Create commit with changes
+# git add .
+# git commit -m "provision superset $unit_name"
 
-# Push changes to GitHub repository
-git push origin master
+# # Push changes to GitHub repository
+# git push origin master

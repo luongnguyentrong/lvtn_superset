c35482dd6669d855f34622deee50bb9ea9fcff1c
Luong <luongnguyentrongduc@gmail.com>
2023-06-02 15:07:10 +0700

    provision superset pdt

--- deploy_new_superset.sh
+++ deploy_new_superset.sh
@@ -6,7 +6,9 @@ unit_name="$1"
 echo "Deploying new superset for unit $unit_name to server..."
 
 # run new superset container
-docker compose up $unit_name -d
+docker compose up superset_$unit_name -d
+docker compose exec superset_$unit_name superset db upgrade
+docker compose exec superset_$unit_name superset init
 
 # restart nginx container
 docker compose down nginx
